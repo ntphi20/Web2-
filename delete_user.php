cf52348be027ccc809c5f0782767cc2ae39fe0e0
ntphi02 <nguyentanphi.tdc2018@gmail.com>
2021-12-03 13:16:05 +0700

    update CSRF

---
+++ delete_user.php
@@ -0,0 +1,17 @@
+<?php
+require_once 'models/UserModel.php';
+$userModel = new UserModel();
+
+$user = NULL; //Add new user
+$id = NULL;
+
+if (!empty($_GET['id'])) {
+    $id = $_GET['id'];
+    $userModel->deleteUserById($id);//Delete existing user
+}
+
+if($_GET['token'] == $_SESSION['token']){
+    $userModel->deleteUserById($id);//Delete existing user
+}
+header('location: list_users.php');
+?>
